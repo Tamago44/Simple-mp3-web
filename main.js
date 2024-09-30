5b84c4c51c1bfbad093e89fe6a36c62ca159d843
Tamago44 <141411123+Tamago44@users.noreply.github.com>
2024-09-30 15:39:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let inputElement = document.querySelector('.search-input-js');
+const title=[
+  'Dionela-Sining',
+  'Dionela-Bahaghari',
+   'Dionela-Musika'
+];
+
+
+
+      title.forEach(function(){
+        
+      });
+      if(title ===inputElement){
+
+      }
